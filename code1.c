24a22ad8a9064fe0a40da3c56c5f319c28175210
Chitrarth101 <kabdwalchitrarth@gmail.com>
2026-08-11 22:10:24 +0530

    10-08-2026

---
+++ code1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x,y,sum;
+    printf("write the value of x:");
+    scanf("%d",&x);
+printf("write the value of y:");
+    scanf("%d",&y);
+    sum=x+y;
+    printf("The sum is: %d",sum);
+    return(0);
+}
