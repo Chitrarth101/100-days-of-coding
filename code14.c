6df72eef57e1837926e2a1de3818e35f4926e5a5
Chitrarth101 <kabdwalchitrarth@gmail.com>
2026-08-16 23:43:55 +0530

    Create code14.c

---
+++ code14.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("write any year:");
+    scanf("%d",& a);
+    if(a%4==0){
+    printf("this is a leap year ");
+    } 
+    else {
+    printf("this is not a leap year ");
+}
+return(0);
+}
