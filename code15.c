29778e92d14204e2f4b604dce55dc0896461e098
Chitrarth101 <kabdwalchitrarth@gmail.com>
2026-08-17 19:56:13 +0530

    Create code15.c

---
+++ code15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+int a,b,c;
+printf("write the value of a:");
+scanf("%d",&a);
+printf("write the value of b:");
+scanf("%d",&b);
+printf("write the value of c:");
+scanf("%d",&c);
+if(a>b && a>c){
+printf("a is the largest number");}
+else if(b>a && b>c){
+printf("b is the largest number");}
+else if (c>a && c>b){
+printf("c is the largest number");
+}
+}
